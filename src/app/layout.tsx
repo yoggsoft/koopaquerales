b4cb829ca9196836486850ef8d0093f9b6455464
yoggsoft <yoggsoft@gmail.com>
2025-02-10 14:59:56 +0100

    add robots, sitemap, og, tcard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,26 @@ const permanentMarkerFont = Permanent_Marker({
 export const metadata: Metadata = {
   title: "Koopa Querales - Official Website",
   description: "Official Website of Koopa Querales.",
+  openGraph: {
+    siteName: "Koopa Querales",
+    title: "Koopa Querales",
+    url: "http://www.koopaquerales.com",
+    type: "website",
+    description: "Official Website of Koopa Querales. Listen on all platforms.",
+    locale: "en_US",
+    images: "/img/opengraph-image.jpg"
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: "@koopaquerales",
+    title: "Koopa Querales - singer-songwriter and guitarist from Venezuela",
+    description: "Official Website of Koopa Querales. Listen on all platforms.",
+    creator: "@koopaquerales",
+    images: "/img/opengraph-image.jpg"
+  }
 };
 
 const woorankAssistant = `
-<script type="text/javascript">
   window.woorankAssistantOptions = window.woorankAssistantOptions || {};
   window.woorankAssistantOptions.url = 'koopaquerales.com';
   window.woorankAssistantOptions.assistantPublicKey = 'fde41ea65dae85425d58fb96391070ce';
@@ -23,8 +39,7 @@ const woorankAssistant = `
     var wl = document.createElement('script'); wl.type = 'text/javascript'; wl.async = true;
     wl.src = 'https://assistant.woorank.com/hydra/assistantLoader.latest.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wl, s);
-  })();
-</script>`;
+  })();`;
 
 export default function RootLayout({
   children,
@@ -39,23 +54,9 @@ export default function RootLayout({
         <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
         <meta name="url" content="http://www.koopaquerales.com" />
         <meta itemProp="url" content="http://www.koopaquerales.com" />
-
-        <meta property="og:site_name" content="Koopa Querales" />
-        <meta property="og:title" content="Koopa Querales" />
-        <meta property="og:url" content="http://www.koopaquerales.com" />
-        <meta property="og:type" content="website" />
-        <meta property="og:description" content="Official Website of Koopa Querales. The single Tomorrow Will Be Flies is available now." />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:image" content="https://pbs.twimg.com/profile_images/1194786810737647617/rBxiNz9S_400x400.jpg" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@koopaquerales" />
-        <meta name="twitter:title" content="Koopa Querales - singer-songwriter and guitarist from Venezuela" />
-        <meta name="twitter:url" content="http://www.koopaquerales.com" />
-        <meta name="twitter:description" content="Koopa Querales is a singer-songwriter and guitarist from Venezuela, whose music is heavily influenced by the '80s and early '00s rock" />
-        <meta name="twitter:creator" content="@koopaquerales" />
-        <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1194786810737647617/rBxiNz9S_400x400.jpg" />
-        {woorankAssistant}
+        <script type="text/javascript">
+          {woorankAssistant}
+        </script>
       </head>
       <body
         className={`${permanentMarkerFont.variable} ${permanentMarkerFont.variable} antialiased`}
